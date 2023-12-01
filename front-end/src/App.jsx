9eb81415133dea6551a5d8e978965ef7bb065d9c
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-12-01 17:07:18 +0530

    front-end : start

---
+++ front-end/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda aut obcaecati officia odio voluptates nam rerum, quas doloribus cumque quidem tempora veniam, nostrum aperiam veritatis vero commodi eum iure. Accusantium.</h1>
+    </>
+  )
+}
+
+export default App
